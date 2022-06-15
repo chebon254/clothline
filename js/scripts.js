2bfde2fdf6755c69f38bb20d3d991f64e2f17581
chebon254 <kelvinchebon90@gmail.com>
2022-06-15 23:56:06 +0300

    Code Changes

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+/* == BACK TO TOP == */
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  /* == BACK TO TOP == */
+/* == BANNER == */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider");
+  var dots = document.getElementsByClassName("slide-rect");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+
+}
+/* == || BANNER == */
+
+
+/* == ANIMATION CSS == */
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 4;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("showreveal");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  /* == ANIMATION CSS == */
